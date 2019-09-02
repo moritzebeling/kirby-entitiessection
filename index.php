ab0161c8fa8ffffbabb9a6cd37250f9c6c8b921f
Moritz Ebeling <moriwaan@Moritzs-iMac.local>
2019-09-02 17:35:24 +0200

    forking pages section from rasteiner

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Kirby\Cms\Blueprint;
 use Kirby\Toolkit\F;
 use Kirby\Toolkit\Str;
 
-Kirby::plugin('rasteiner/k3-pagesdisplay-section', [
+Kirby::plugin('moritzebeling/kirgby-entitiessection', [
     'sections' => [
         'pagesdisplay' => require __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'PagesDisplaySection.php'
     ]
